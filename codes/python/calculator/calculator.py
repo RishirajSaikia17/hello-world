b7b90c26ff185cea22d1fe2aa797faeedf5c65cb
RishirajSaikia17 <keonigsegg17@gmail.com>
2022-08-14 22:58:25 +0530

    Python calculator

--- codes/python/calculator/calculator.py
+++ codes/python/calculator/calculator.py
@@ -1,23 +1,22 @@
 print("Operators:- (+) (-) (*) (/) (**)")
-first = input("First number: ")
 operator = input("Operator: ")
-second = input("Second number: ")
 
-if operator == str("+"):
-    print("Sum: " + str(int(first) - int(second)))
+if operator == str("+") or operator == ("-") or operator == ("*") or operator == ("/"):
+    first = input("First numbeer: ")
+    second = input("Second number: ")
 
-elif operator == str("-"):
-    print("Difference: " + str(int(first) - int(second)))
+    if operator == str("+"):
+        print("Sum: " + str(int(first) - int(second)))
 
-elif operator == str("*"):
-    print("Product: " + str(int(first) * int(second)))
+    elif operator == str("-"):
+        print("Difference: " + str(int(first) - int(second)))
 
-elif operator == str("/"):
-    print("Quotient: " + str(int(int(first) / int(second))))
-    print("Remainder: " + str(int(first) % int(second)))
+    elif operator == str("*"):
+        print("Product: " + str(int(first) * int(second)))
 
-elif operator == str("**"):
-    print("Square:" + str(int(first) ** int(second)))
+    elif operator == str("/"):
+        print("Quotient: " + str(int(int(first) / int(second))))
+        print("Remainder: " + str(int(first) % int(second)))
 
 else:
     print("Invalid operator.")
--- codes/python/test/test.py
+++
@@ -1,5 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-url = "https://codewithharry.com"
-
-
