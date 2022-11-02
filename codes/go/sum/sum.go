57880c838037536bb22dc21e970fc345cf9dc215
RishirajSaikia17 <84188056+RishirajSaikia17@users.noreply.github.com>
2022-11-02 12:02:03 +0530

    wsl

---
+++ codes/go/sum/sum.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b, c int
+	fmt.Print("Enter the first number: ")
+	fmt.Scan(&a)
+	fmt.Print("Enter the second number: ")
+	fmt.Scan(&b)
+	c = a + b
+	fmt.Println(c)
+}
